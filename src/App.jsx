0508d983a6e9c4a14149a73ef03904809e57457d
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-08-31 19:11:42 +0530

    started making personal portfoilio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <ul>
+      <li><a href="about.html">About</a></li>
+      <li><a href="Resume.html">Resume</a></li>
+      <li><a href="Contact.html">Contact</a></li>
+      <li><a href="Lang.html">Lang</a></li>
+    </ul>
+    <div className='nameSection'>
+      <h1 className='name'>शाश्वत</h1>
+      <p>Web and iOS app deveopler.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+export const Navbar = () => {
+  return (
+   <>
+     <ul>
+      <li><a href="about.html">About</a></li>
+      <li><a href="Resume.html">Resume</a></li>
+      <li><a href="Contact.html">Contact</a></li>
+      <li><a href="Lang.html">Lang</a></li>
+    </ul>
+   </>
+  )
+}
+export default Navbar
