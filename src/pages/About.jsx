668a735fa13bf224572285342f7166984a59d079
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-10-04 12:23:17 +0530

    updated aboutpage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,14 +8,21 @@ export const About = () => {
       <div className="content">
         <img src={shashwat} alt="Shashwat" />
         <div className="text">
-          <p>hi!👋 my name is Kumar Shashwat. I m a prefinal year Computer science student doing my
+          <p className='greet'>HELLO 👋 </p>
+          <p className='description'>
+          My name is <div className='myname'>Kumar Shashwat.</div> <br></br>
+          I m a prefinal year Computer science student doing my
             Bachlor of Technology. I m a web and iOS app developer. I love to create apps. I have also done freelancing work.
-            My responsibility involves crafting websites that are not only functional and user-friendly but also visually appealing. I am a diligent, exceptionally creative, and reliable student committed to improving society. I thrive on fostering a positive atmosphere in my surroundings.
-          </p>
+            My responsibility involves crafting websites that are not only functional and user-friendly but also visually appealing. <br></br>I am a diligent, exceptionally creative, and reliable student committed to improving society. I thrive on fostering a positive atmosphere in my surroundings.
+            </p>
         </div>
       </div>
      <div className="do">
-      <h1>what i do</h1>
+      <h1>What I Doing</h1>
+      <div className='ios'></div>
+      <div className='web'></div>
+      <div className='design'></div>
+
      </div>
     </>
   )
