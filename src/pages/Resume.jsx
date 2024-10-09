900203a02186ef018317b8a596131d3cec1b8f22
shashwat41 <shashwatrajwanshi41@gmail.com>
2024-10-10 05:10:20 +0530

    updated ui

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ import resumeicon from '../assets/download_resume.png'
 import resume from "../assets/resume.pdf";
 import CircleWithNumber from '../components/CirclewithNumber';
 import SkillsSection from '../components/SkillsSection';
-
+import redirect from '../assets/maximize.png'
 
 const Resume = () => {
   return (
@@ -12,57 +12,60 @@ const Resume = () => {
       <div className="box">
         {/* <h1>RESUME</h1> */}
         <div className="head">
-          <h2>RESUME</h2>
+          <h1>RESUME</h1>
           <div>
             <a href={resume} download="shashwat_resume"><img src={resumeicon} alt="resumeicon" /></a>
           </div>
         </div>
-        <div className="content resumemain">
+        <div className="resumemain">
           <div className="education">
-            <div className="subheading">
-              <h3>Education</h3>
-            </div>
+          <h2>Education</h2>
             <div className="edu">
               <CircleWithNumber count={0} />
               <div className="eduinfo">
                 ABES ENGINNERING COLLEGE
-                <p>2022-2024</p>
+                <p>2022-2026</p>
               </div>
             </div>
             <div className="edu">
               <CircleWithNumber count={1} />
               <div className="eduinfo">
                 Trinity Global School
-                <p>2022-2024</p>
+                <p>2018-2019</p>
               </div>
             </div>
             <div className="edu">
               <CircleWithNumber count={2} />
               <div className="eduinfo">
                 St Mary High School
-                <p>2022-2024</p>
+                <p>2007-2018</p>
               </div>
             </div>
           </div>
-          <div className="experience">
-            <div className="subheading">
-              <h3>Projects</h3>
-              <p className='experiencetxt'>
-                <h3><a href="">WEATHERAPP</a></h3>Swift, SwiftUI, openweathermap, Xcode, Json, Api integration. <br />
-                WeatherApp is a sleek, intuitive weather application designed to keep users informed about current weather conditions
-                and forecasts. Built with Swift, SwiftUI, and leveraging the OpenWeatherMap API, WeatherWise exemplifies modern
-                iOS app development, this app provides real-time weather updates and detailed forecasts, all through a beautiful and
-                user-friendly interface. <br /> <br/>
-                <h3><a href="">GUESS THE FLAG</a> </h3>Swift,UIKIT,Xcode,Storyboard,Cocoa Touch <br/>
-                ”Guess the Flag” is an engaging and educational app designed to test and improve your knowledge of world flags. With an
-                intuitive interface and a vast collection of flags from around the globe, this app provides an entertaining way to learn
-                about different countries and their flags.Built with Swift, UIKit, Xcode, and Storyboard.
-              </p>
-            </div>
+          {/* put the componet beloew me */}
+          <SkillsSection />
+        </div>
+
+        {/* put the div section below me */}
+        <div className="project">
+        <h1>Projects</h1>
+          <div className="subheading">
+            
+            <p className='projectetxt'>
+              <h3> <a href="https://github.com/shashwat329/swiftprojects"> WEATHERAPP<img src={redirect} alt="redirecticon" /></a> </h3>
+               <h4>Swift, SwiftUI, openweathermap, Xcode, Json, Api integration.</h4>
+              WeatherApp is a sleek, intuitive weather application designed to keep users informed about current weather conditions
+              and forecasts. Built with Swift, SwiftUI, and leveraging the OpenWeatherMap API, WeatherWise exemplifies modern
+              iOS app development, this app provides real-time weather updates and detailed forecasts, all through a beautiful and
+              user-friendly interface. <br /> <br />
+              <h3> <a href="https://github.com/shashwat329/swiftprojects">GUESS THE FLAG<img src={redirect} alt="redirecticon" /> </a> </h3> 
+              <h4>Swift,UIKIT,Xcode,Storyboard,Cocoa Touch</h4>
+              ”Guess the Flag” is an engaging and educational app designed to test and improve your knowledge of world flags. With an
+              intuitive interface and a vast collection of flags from around the globe, this app provides an entertaining way to learn
+              about different countries and their flags.Built with Swift, UIKit, Xcode, and Storyboard.
+            </p>
           </div>
         </div>
-        <SkillsSection />
-
       </div >
     </>
   )
