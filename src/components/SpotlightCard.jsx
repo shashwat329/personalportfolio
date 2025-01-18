2c0774322902c014dc6db9125e9fcffce1572c7b
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-01-18 13:28:22 +0530

    renamed a file

--- src/components/SpotlightCard.jsx
+++ src/components/SpotlightCard.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import "../style/spotlightCard.css";
+import "../style/SpotlightCard.css";
 
 const SpotlightCard = ({ children, className = "", spotlightColor = "rgba(255, 255, 255, 0.25)" }) => {
   const divRef = useRef(null);
