edfd085bde77c1af823d2223ec4b0dea9896ee83
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-04-16 12:30:52 +0530

    updated the SEO

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <>
     
   <Router> 
-      <Navbar /> 
+      <Navbar/> 
       <Routes>
       <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
